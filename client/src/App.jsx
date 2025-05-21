28c7f0157a00d889730a4e55041245f30f14cc5d
Roger Ancamilla <roger.ancamilla@gmail.com>
2025-05-21 19:18:50 -0400

    se agregó carousel al diseño

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './views/home/Home.jsx'
 import Blog from './views/blog/Blog.jsx'
 import Coleccion from './views/coleccion/Coleccion.jsx'
+import Carousel from './components/carousel/Carousel.jsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-         <Route path="/coleccion" element={<Coleccion/>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Carousel/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/coleccion" element={<Coleccion/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
