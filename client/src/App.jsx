99c6427fb4a8eabd67ffc69442ab31fe209a26f1
Roger Ancamilla <roger.ancamilla@gmail.com>
2025-06-02 22:29:11 -0400

    WIP: Cambios locales en App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './views/home/Home.jsx'
-import Blog from './views/blog/Blog.jsx'
 import Coleccion from './views/coleccion/Coleccion.jsx'
 import Carrito from './views/carrito/Carrito.jsx'
 import Navbar from './components/navbar/Navbar.jsx'
@@ -16,7 +15,6 @@ function App() {
        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
           <Route path="/coleccion" element={<Coleccion/>} />
           <Route path="/carrito" element={<Carrito/>} />
         </Routes>
