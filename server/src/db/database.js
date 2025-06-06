9080cffaa3e4ed0b3fb1fa6d219ca086bcb45426
Roger Ancamilla <roger.ancamilla@gmail.com>
2025-06-06 08:57:10 -0400

    se comenzó a desarrollar el backend con modelo vista controlador. Se instaló paquete pg y se extrajo método pool. Se conectó la bdd

---
+++ server/src/db/database.js
@@ -0,0 +1,30 @@
+//extrajimos método Pool del paquete pg// 
+const {Pool} = require("pg");
+
+//importamos las variables de entorno//
+require("dotenv").config({path:"./.env"});
+
+//conectamos la BDD con el método POOL del paquete pg//
+class database {
+    constructor(){
+        this.pool = new Pool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            allowExitOnIdle: true,
+        });
+        this.pool.on("error", (err) => {
+        
+            this.handleError(err);
+        });
+    }
+    handleError(err){
+        console.error("error en la conexión", err);
+    }
+
+    query(sql, params){
+        return this.pool.query(sql, params);
+    }
+};
